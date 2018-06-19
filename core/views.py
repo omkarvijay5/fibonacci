b71356fe62de4dc1cb6cfe3c16ce13aa9807fe13
omkarvijay5 <omkarvijay5@gmail.com>
2018-06-19 13:34:29 +0530

    nth fibonaccii series dummy view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
 
-# Create your views here.
+# django imports
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def nth_number(request):
+	 return HttpResponse("Fibonaccii series")
